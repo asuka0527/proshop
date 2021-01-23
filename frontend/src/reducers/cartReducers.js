38c6749fb6ffd8af6e3f44c78597dfd46ef3818d
asuka0527 <junizzanakajima@gmail.com>
2021-01-23 15:37:38 +0900

    CArt reducer and Add to cart Action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM: {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find(
+        (existing) => existing.product === item.product
+      );
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((cartItem) =>
+            cartItem.product ? item : cartItem
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    }
+    default:
+      return { state };
+  }
+};
