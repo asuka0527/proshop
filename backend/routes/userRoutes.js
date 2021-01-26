82b1b49b62fa42847f6528177ca35df8752561f4
asuka0527 <junizzanakajima@gmail.com>
2021-01-26 17:52:38 +0900

    Generate a JSON Web Token and Custom Authentication Middlware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 
-import { authUser } from "../controllers/userControllers.js";
+import { authUser, getUserProfile } from "../controllers/userControllers.js";
 
-// [Fetching productcs from DATABASE] -3).  setup user router
+import { protect } from "../middleware/authMiddleware.js";
+
+// [Fetching users from DATABASE] -3).  setup user router
 const router = express.Router();
 
 router.post("/login", authUser);
+
+// we use route because we are gonna make a GET & PUT request to update user profile
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
